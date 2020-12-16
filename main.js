f4864df863b6a20e2a4387c3d0f5cdb4a76b45a6
Roma Levsha <rm.levsha@gmail.com>
2020-12-16 15:53:07 +0300

    fix: correct mistake style activatable

--- main.js
+++ main.js
@@ -23,14 +23,12 @@ const Keyboard = {
     this.elements.keysContainer.classList.add('keyboard__keys');
     this.elements.keysContainer.appendChild(this._createKeys());
 
-    this.elements.keys = this.elements.keysContainer.querySelectorAll(
-      '.keyboard__key'
-    );
+    this.elements.keys = this.elements.keysContainer.querySelectorAll('.keyboard__key');
 
     this.elements.main.appendChild(this.elements.keysContainer);
     document.body.appendChild(this.elements.main);
 
-    document.querySelectorAll('.use-keyboard-input').forEach((element) => {
+    document.querySelectorAll('.use-keyboard-input').forEach(element => {
       element.addEventListener('focus', () => {
         this.open(element.value, (currentValue) => {
           element.value = currentValue;
@@ -94,7 +92,7 @@ const Keyboard = {
     ];
 
     const createIconHTML = (icon_name) => {
-      return `<i class='material-icons'>!${icon_name}</i>`;
+      return `<i class='material-icons'>${icon_name}</i>`;
     };
 
     keyLayout.forEach((key) => {
